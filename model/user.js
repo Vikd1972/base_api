f0dc5646ae5855abc6f6e83befa6659f7faaf285
-add <d_v_o@mail.ru>
2021-08-26 16:33:51 +0300

    Full CRUD per user without authentication

--- model/user.js
+++ model/user.js
@@ -3,10 +3,15 @@ const Sequelize = require("sequelize");
 //var crypto = require("crypto");
 
 const sequelize = new Sequelize("user2db", "user2", "root1234", {
-  dialect: "postgres"
- });
+  dialect: "postgres",
+  host: "localhost",
+  define: {
+    timestamps: false,
+  },
+});
 
-const User = sequelize.define("user", {
+
+const User = sequelize.define("userdb", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
@@ -18,13 +23,18 @@ const User = sequelize.define("user", {
     allowNull: false,
   },
   email: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     allowNull: false,
   },
   dob: {
-    type: Sequelize.DATE,
+    type: Sequelize.DATEONLY,
     allowNull: false,
   },
 });
+
+sequelize
+  .sync()
+  .then((result) => console.log('База синхронизирована'))
+  .catch((err) => console.log(err));
 
 module.exports = User;
--- route/user.js
+++ route/user.js
@@ -1,30 +1,16 @@
+const { response } = require("express");
 const express = require("express");
-const Sequelize = require("sequelize");
+//const Sequelize = require("sequelize");
 const router = express.Router();
 
 const User = require("../model/user");
-
-
-router.post("/search", (req, res) => {
-  User.findOne({ email: req.body.email }, function (err, user) {
-    if (user === null) {
-      return res.status(400).send({
-        message: "User not found.",
-      });
-    } else {
-      return res.status(201).send({
-        message: "User found",
-      });
-    }
-  });
-});
-
 
 router.post("/create", (req, res) => {
   if (!req.body) return res.sendStatus(400);
   const usern = req.body.fullname;
   const usere = req.body.email;
   const userd = req.body.dob;
+
   User.create({ fullname: usern, email: usere, dob: userd })
     .then(() => {
       res.redirect("/");
@@ -32,47 +18,51 @@ router.post("/create", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-router.post("/edit", (req, res) => {
-  if (!req.body) return res.sendStatus(400);
-  const usern = req.body.fullname;
-  const usere = req.body.email;
-  const userd = req.body.dob;
-
-  User.findOne({ email: req.body.oldemail }, function (err, user) {
-    if (user === null) {
-      return res.status(400).send({
-        message: "User not found.",
-      });
-    } else {
-      userid = user.id;
-    }
-  });
-
-  User.update(
-    { fullname: usern, email: usere, dob: userd },
-    { where: { id: userid } }
-  )
-    .then(() => {
-      res.redirect("/users");
-    })
-    .catch((err) => console.log(err));
-});
-
-
-router.post("/delete", (req, res) => {
+router.delete("/delete", (req, res) => {
   const usere = req.body.email;
   User.destroy({ where: { email: usere } })
     .then(() => {
-      res.redirect("/users");
+      res.redirect("/");
     })
     .catch((err) => console.log(err));
 });
 
+router.put("/search", (req, res) => {
+  const usere = req.body.email;
+  User.findOne({ where: { email: usere } })
+    .then((user) => {
+      if (!user) return;
+    })
+    .catch((err) => console.log(err));
+});
 
-router.post("/users", (req, res) => {
+router.post("/edit", (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  const oldusere = req.body.oldemail;
+  const usern = req.body.fullname;
+  const usere = req.body.email;
+  const userd = req.body.dob;
+  let userid = null;
+
+  User.findOne({ where: { email: oldusere } })
+    .then((user) => {
+      if (!user) return;
+      userid = user.id;
+      User.update(
+        { fullname: usern, email: usere, dob: userd },
+        { where: { id: userid } }
+      ).then((res) => {
+        console.log(res);
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/", (req, res) => {
   User.findAll({ raw: true })
     .then((users) => {
       console.log(users);
+      return;
     })
     .catch((err) => console.log(err));
 });
